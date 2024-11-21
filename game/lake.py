dd88a4fd678c335f1663387b1b98824ac0ecc082
deyme17 <dmytro19022006@gmail.com>
2024-11-21 15:57:58 +0200

    одано клас Lake для управління станом озера

---
+++ game/lake.py
@@ -0,0 +1,20 @@
+import random
+
+
+class Lake:
+    def __init__(self):
+        self.water_quality = 0  # Початковий рівень якості води (0 - нейтральний)
+
+    def update_quality(self, effect):
+        self.water_quality += effect
+
+    def get_quality(self):
+        return self.water_quality
+
+    def flood_event(self, min_clean=2, max_clean=12):
+        clean_amount = random.randint(min_clean, max_clean)
+        self.update_quality(clean_amount)
+        return clean_amount
+
+    def __str__(self):
+        return f"Якість води: {self.water_quality}"

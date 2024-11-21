5264005ed1de3f4e9fc97d6c6e7a994b5c580e0f
deyme17 <dmytro19022006@gmail.com>
2024-11-22 00:25:28 +0200

    переробив мейн

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, ConversationHandler, MessageHandler, filters
-from bot.handlers import start, enter_name, help_command, rule_command
+from bot.handlers import start, help_command, rule_command
+from bot.controller import start_game, end_game
 from config.secrets import TOKEN
+
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
 
+    # Реєстрація обробників
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            0: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_name)],
-        },
+        entry_points=[CommandHandler("start", start)],
+        states={},
         fallbacks=[],
     )
 
-    application.add_handler(CommandHandler('help', help_command))
-    application.add_handler(CommandHandler('rule', rule_command))
     application.add_handler(conv_handler)
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("rule", rule_command))
+    application.add_handler(CommandHandler("start_game", start_game))
+    application.add_handler(CommandHandler("end_game", end_game))
 
     application.run_polling()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

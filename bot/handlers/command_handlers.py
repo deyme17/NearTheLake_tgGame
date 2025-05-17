b6b6830931a16193f65d64d9f26ff2ef23e8407e
dmytro <dmytro19022006@gmail.com>
2025-05-17 22:51:02 +0300

    fixes

--- bot/handlers/command_handlers.py
+++ bot/handlers/command_handlers.py
@@ -5,9 +5,17 @@ from bot.services.registrar_service import PlayerRegistrar
 from bot.controllers.info_controller import InfoController
 from bot.controllers.game_controller import GameController
 
+from bot.ui_components.keyboard import get_keyboard_for_state
 from config.helpers import get_game
+from messages.start_message import greeting_menu_messsge
 
 async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text(
+        greeting_menu_messsge,
+        reply_markup=get_keyboard_for_state("idle")
+    )
+
+async def start_game_command(update: Update, context: CallbackContext):
     game = get_game(context)
     await PlayerRegistrar.handle_start(update, context, game)
 

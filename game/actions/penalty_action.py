39b508846ecb7e6343e958f41ead138ef8906fb4
dmytro <dmytro19022006@gmail.com>
2025-05-17 16:20:08 +0300

    bugFix

--- game/actions/penalty_action.py
+++ game/actions/penalty_action.py
@@ -4,4 +4,4 @@ class PenaltyAction(BaseAction):
     """Class of the action 'penalty'"""
     def apply(self, game, player):
         penalty_cost = -len(game.players)
-        player.add_score(penalty_cost)
+        player.add_points(penalty_cost)

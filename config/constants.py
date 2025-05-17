bdf18f1427977112ab11eeeec2d5a4981745f3e3
dmytro <dmytro19022006@gmail.com>
2025-05-17 14:16:28 +0300

    add constants/updates

---
+++ config/constants.py
@@ -0,0 +1,15 @@
+# Ідентифікатори дій
+ACTION_DUMP = "1"
+ACTION_CLEAN = "2"
+ACTION_PENALTY = "3"
+ACTION_BONUS = "4"
+
+# Назви дій для кнопок і виводу
+ACTION_NAMES = {
+    ACTION_DUMP: "Скидання",
+    ACTION_CLEAN: "Очищення",
+    ACTION_PENALTY: "Штраф",
+    ACTION_BONUS: "Премія"
+}
+
+game_matrix_path = "assets/gameMatrix_NearTheLake.png"

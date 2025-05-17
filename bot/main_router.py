be55e39d33817fec0268e1261fad6359df612281
dmytro <dmytro19022006@gmail.com>
2025-05-17 22:49:19 +0300

    add route

--- bot/main_router.py
+++ bot/main_router.py
@@ -1,5 +1,5 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-from bot.handlers.command_handlers import start, help_command, rule_command, end_game_command
+from bot.handlers.command_handlers import start, help_command, rule_command, end_game_command, start_game_command
 from bot.handlers.action_handlers import handle_action_callback
 from bot.handlers.meeting_handlers import handle_end_meeting_vote
 from bot.handlers.message_handlers import handle_player_message
@@ -19,10 +19,11 @@ def create_app():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("rule", rule_command))
+    application.add_handler(CommandHandler("start_game", start_game_command))
     application.add_handler(CommandHandler("end_game", end_game_command))
     application.add_handler(CommandHandler("action", prompt_action))
     application.add_handler(CallbackQueryHandler(handle_action_callback, pattern=r"^\d$"))
     application.add_handler(CallbackQueryHandler(handle_end_meeting_vote, pattern="end_meeting_vote"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_player_message))  # <== Додай це
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_player_message))
 
     return application

ad67cfd7f48279f2e82d96dbb20f03601a35aff9
dmytro <dmytro19022006@gmail.com>
2025-05-17 22:49:54 +0300

    create command managers

---
+++ bot/command_managers/idle_command_manager.py
@@ -0,0 +1,21 @@
+from bot.commands.command import BaseCommand
+from bot.commands.rule_command import ShowRulesCommand
+from bot.commands.start_command import StartGameCommand
+from bot.commands.settings_command import SettingsCommand
+from messages.error_messages import command_not_recognize_message
+
+class IdleStateCommandManager:
+    def __init__(self):
+        self.commands: list[BaseCommand] = [
+            ShowRulesCommand(),
+            StartGameCommand(),
+            SettingsCommand()
+        ]
+
+    async def handle(self, text, update, context, game):
+        for command in self.commands:
+            if command.matches(text):
+                await command.execute(update, context, game)
+                return
+
+        await update.message.reply_text(command_not_recognize_message)

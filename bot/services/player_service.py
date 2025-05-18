bb2c9c08b2ba355901fec460fe87f71354d6e5ac
dmytro <dmytro19022006@gmail.com>
2025-05-18 19:51:12 +0300

    new services

--- bot/services/player_service.py
+++ bot/services/player_service.py
@@ -4,15 +4,12 @@ from messages.state_messages import (
 )
 from game.gamelogic.game_engine import GameEngine
 from bot.services.state_service import StateService
-from bot.services.ui_refresh_service import UIRefreshService
-
+from bot.services.keyboard_messenger import KeyboardMessenger 
+from bot.services.message_broadcast_service import MessageBroadcastService
 
 class PlayerService:
     @staticmethod
     async def register(update, context, game):
-        """
-        Returns: success: bool, current_count: int or None
-        """
         if not game:
             await update.message.reply_text(game_not_configure_message)
             return False, None
@@ -25,28 +22,24 @@ class PlayerService:
         user_name = update.effective_user.first_name
 
         success, current_count = game.add_player(user_id, user_name)
-        if success:
-            StateService.set_state(context, user_id, "waiting_lobby")
-
-            await UIRefreshService.update_keyboard(
-                bot=context.bot,
-                chat_id=user_id,
-                text=joined_message(user_name, current_count, game),
-                state="waiting_lobby"
-            )
-
-            for player in game.players.values():
-                if player.player_id != user_id:
-                    await context.bot.send_message(
-                        chat_id=player.player_id,
-                        text=player_connected_message(user_name, current_count, game)
-                    )
-
-            if current_count == game.settings.max_players:
-                await GameEngine(game, context).start_game()
-
-            return True, current_count
-
-        else:
+        if not success:
             await update.message.reply_text(game_full_message)
             return False, None
+
+        StateService.set_state(context, user_id, "waiting_lobby")
+
+        await KeyboardMessenger.send(
+            bot=context.bot,
+            chat_id=user_id,
+            text=joined_message(user_name, current_count, game),
+            state="waiting_lobby"
+        )
+
+        await MessageBroadcastService.send_all_except(
+            bot=context.bot,
+            players=game.players,
+            excluded_id=user_id,
+            text=player_connected_message(user_name, current_count, game)
+        )
+
+        return True, current_count
